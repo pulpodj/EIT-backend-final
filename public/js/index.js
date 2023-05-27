1c689cfa26ef9953756c2fc1627d88c7102a08ff
Nicolas Muller <muller.nic@gmail.com>
2023-05-27 20:23:21 -0300

    Primer prueba en render

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 const cardContainer = document.getElementById('#card-container');
 const btnCloseCategory = document.getElementById("category-btn-close");
 let Products = [];
-const URL = 'http://localhost:13000';
+const URL = 'https://eit-backend-final.onrender.com/';
 
 
 async function cargarProductos(){
